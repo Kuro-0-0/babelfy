1a568817ee07847ebc7b3bf23498e5b773b82381
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-02-18 10:20:09 +0100

    Update Song.java
    Por ahora Songs debe de estar vacio

--- babelfy/src/main/java/com/babel/babelfy/model/Song.java
+++ babelfy/src/main/java/com/babel/babelfy/model/Song.java
@@ -1,19 +1,4 @@
 package com.babel.babelfy.model;
 
-import java.time.LocalDate;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-@Entity
 public class Song {
-
-    @Id
-    private long id;
-    private String name;
-    private int duration;
-    private String artistName;
-    private String albumName;
-    private LocalDate releaseDate;
-
 }
