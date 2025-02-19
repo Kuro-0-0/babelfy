d0c5308c8cc3efc6e5e7162db0d9b3a618157d16
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-19 11:12:46 +0100

    Front end

--- front/js/deleteCategories.js
+++ front/js/deleteCategories.js
@@ -1,7 +1,44 @@
-function alternar() {
-    if (document.getElementById("deleteCategories").style.display == "block") {
-        document.getElementById("deleteCategories").style.display = "none"
-    } else {
-        document.getElementById("deleteCategories").style.display = "block"
-    }
+function deleteCategory() {
+    let options;
+
+    options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/JSON', 
+        },
+    };
+    fetch('http://localhost:9000/categories/'+localStorage.getItem('idCategory'), options)
+        .then(response => {
+            console.log(response);
+            return response
+        })
+        .then (respuesta => {
+            showActionBTN()
+            if (respuesta.status == 200) {
+                estado = 'Success'
+            } else {
+                estado = 'Error'
+            }
+            return respuesta.text()
+        })
+        .then(text => {
+            showPopUp(estado,text)
+            window.location.href = 'CategoriesList.html'
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
+
+function showActionBTN() {
+    const deleteCategories = document.getElementById("PopUpDelete");
+    const deleteBtn = document.getElementById("deleteBtn");
+  
+    if (deleteCategories.style.display === "block") {
+        deleteCategories.style.display = "none";
+        deleteBtn.disabled = false; 
+    } else {
+        deleteCategories.style.display = "block";
+        deleteBtn.disabled = true; 
+    }
+  }
