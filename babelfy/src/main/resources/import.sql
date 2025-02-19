8587211a570e43276833fea299f5d133f00d3317
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-19 14:45:38 +0100

    Remove hex from import

--- babelfy/src/main/resources/import.sql
+++ babelfy/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO category(id, name, hex_color) VALUES (1, 'Rock', 'F0FF7A');
-INSERT IGNORE INTO category(id, name, hex_color) VALUES(2, 'Pop', '058FAC');
-INSERT IGNORE INTO category(id, name, hex_color) VALUES (3, 'Reggae', '91CF87');
+INSERT IGNORE INTO category(id, name) VALUES (1, 'Rock');
+INSERT IGNORE INTO category(id, name) VALUES(2, 'Pop');
+INSERT IGNORE INTO category(id, name) VALUES (3, 'Reggae');
