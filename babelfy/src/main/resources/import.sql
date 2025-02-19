e747f29d3a301f0478580ffc942d5e58062f7fec
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-19 14:01:32 +0100

    Change SQL import mode

--- babelfy/src/main/resources/import.sql
+++ babelfy/src/main/resources/import.sql
@@ -1,6 +1,3 @@
--- Inserting into category table
-INSERT IGNORE INTO category(id, name, hex_color)
-VALUES
-    (1, 'Rock','F0FF7A'),
-    (2, 'Pop','058FAC'),
-    (3, 'Reggae','91CF87');
+INSERT IGNORE INTO category(id, name, hex_color) VALUES (1, 'Rock', 'F0FF7A');
+INSERT IGNORE INTO category(id, name, hex_color) VALUES(2, 'Pop', '058FAC');
+INSERT IGNORE INTO category(id, name, hex_color) VALUES (3, 'Reggae', '91CF87');
