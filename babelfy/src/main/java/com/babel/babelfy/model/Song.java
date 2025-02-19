eafb1ea41eb16cb117c24e73d33c2bb4dc6485ca
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-19 14:03:02 +0100

    Remove SOngs

--- babelfy/src/main/java/com/babel/babelfy/model/Song.java
+++ babelfy/src/main/java/com/babel/babelfy/model/Song.java
@@ -3,11 +3,6 @@ package com.babel.babelfy.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
 public class Song {
-
-    @Id
-    private long id;
-    private String name;
 
 }
