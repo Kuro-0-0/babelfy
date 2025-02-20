7f49697f02c24669a570424deef69aed27359493
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-20 12:31:54 +0100

    Thumbnail done

--- babelfy/src/main/java/com/babel/babelfy/repository/CategoryRepository.java
+++ babelfy/src/main/java/com/babel/babelfy/repository/CategoryRepository.java
@@ -5,12 +5,11 @@ import com.babel.babelfy.model.Category;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findByName(String name);
-
-
 }
