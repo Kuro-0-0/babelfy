892449ea6332010cea7f16e7801622de2f37e9b3
pablo.garciam <pablo.garciam@babelgroup.com>
2025-02-26 12:51:31 +0100

    reADD details

--- babelfy/src/main/java/com/babel/babelfy/dto/song/SongDtoResponseDetails.java
+++ babelfy/src/main/java/com/babel/babelfy/dto/song/SongDtoResponseDetails.java
@@ -10,19 +10,34 @@ import java.time.LocalDate;
 @Builder
 public class SongDtoResponseDetails {
 
+
     private String name;
     private int duration;
     private String artistName;
     private String albumName;
     private LocalDate releaseDate;
+    private String categoryName;
+    private long categoryID;
 
     public static SongDtoResponseDetails songToCSongDTO(Song s) {
+        String categoryName;
+        long categoryID;
+
+        if (s.getCategory() != null ) {
+            categoryName = s.getCategory().getName();
+            categoryID = s.getCategory().getId();
+        } else {
+            categoryName = "None";
+            categoryID = -1;
+        }
         return SongDtoResponseDetails.builder()
                 .name(s.getName())
                 .duration(s.getDuration())
                 .artistName(s.getArtistName())
                 .albumName(s.getAlbumName())
                 .releaseDate(s.getReleaseDate())
+                .categoryName(categoryName)
+                .categoryID(categoryID)
                 .build();
     }
 
